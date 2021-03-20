265e5f71f32b7378cdcafebff0f11c5ca6094bb5
Shreya Malogi <5hre9a@gmail.com>
2021-03-20 13:36:59 +0530

    Update hirst_painting.py

--- hirst_painting.py
+++ hirst_painting.py
@@ -11,7 +11,7 @@ myrtle.speed("fastest")
 myrtle.penup()
 myrtle.hideturtle()
 
-# to get the colors we need to extract the RGB value from the image using color.py
+# to get the colors we need to extract the RGB value from the image using colorgram.py
 color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
 
 myrtle.setheading(225)
